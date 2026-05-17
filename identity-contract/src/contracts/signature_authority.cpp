8d4f3fe43b21ad69183092d04f70a9d76565487d
hasan7n <hasankassim7@hotmail.com>
2026-05-17 14:39:16 +0200

    add wallet methods to signature authority

--- identity-contract/src/contracts/signature_authority.cpp
+++ identity-contract/src/contracts/signature_authority.cpp
@@ -62,6 +62,9 @@ contract_method_reference_t contract_method_dispatch_table[] = {
     CONTRACT_METHOD2(describe_signing_context, ww::identity::identity::describe_signing_context),
     CONTRACT_METHOD2(sign, ww::identity::identity::sign),
     CONTRACT_METHOD2(verify, ww::identity::identity::verify),
+    CONTRACT_METHOD2(add_vc, ww::identity::identity::add_vc),
+    CONTRACT_METHOD2(get_vc_list, ww::identity::identity::get_vc_list),
+    CONTRACT_METHOD2(get_vp, ww::identity::identity::get_vp),
 
     CONTRACT_METHOD2(sign_credential, ww::identity::signature_authority::sign_credential),
     CONTRACT_METHOD2(verify_credential, ww::identity::signature_authority::verify_credential),
