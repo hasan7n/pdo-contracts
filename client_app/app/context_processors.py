b2b4089bc3114a0cc256c174af5d2f3029ca78b8
hasan7n <hasankassim7@hotmail.com>
2026-05-17 14:39:27 +0200

    remove client app from repo

--- client_app/app/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import AppConfig
-
-
-def app_context(request):
-    config = AppConfig.get_instance()
-    return {
-        'app_config': config,
-        'current_identity': config.public_key,
-    }
